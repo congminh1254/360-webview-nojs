5285378eb60db8a645c45ac49716d9aeb9b4cce3
Minh Nguyen Cong <mcong@box.com>
2023-04-12 02:25:43 +0200

    Change port

--- Dockerfile.sh
+++ Dockerfile.sh
@@ -2,4 +2,4 @@
 
 docker stop 360-webview && docker rm 360-webview
 docker build . -t 360-webview
-docker run -d -it --name 360-webview --restart always -p 9091:9091 360-webview
+docker run -d -it --name 360-webview --restart always -p 9001:9001 360-webview
